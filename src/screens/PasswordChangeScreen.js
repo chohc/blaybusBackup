89ecf72a13e3a13b0713a8be063ba2b06dcd4fc1
chohc <hyej5501@naver.com>
2025-01-15 04:21:30 +0900

    비밀번호 변경 화면

--- src/screens/PasswordChangeScreen.js
+++ src/screens/PasswordChangeScreen.js
@@ -1,141 +1,235 @@
-import React from "react";
+import React, { useState } from "react";
+import "../App.css";
+import arrowback from "../images/arrow_back.png";
+import visibility from "../images/visibility.png";
+import visibilityoff from "../images/visibility_off.png";
+import colors from "../colors/colors";
 import "../fonts/font.css";
-import gif1 from "../images/home/gif1.gif";
-import HomeTree from "../images/home/HomeTree.png";
-import dart from "../images/exp/exp_dart.png";
+import { customAxios } from "../customAxios";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import PressableButton from "../components/PressableButton";
 
-const HomeScreen = () => {
-  const styles = {
-    scrollView: {
-      background: "linear-gradient(180deg, #FFB09E, #FFECE7)",
-      paddingTop: "19px",
-    },
-    column: {
-      alignSelf: "stretch",
-      background: "#FFFFFF",
-      borderRadius: "16px",
-      padding: "20px",
-      marginBottom: "30px",
-      marginLeft: "17px",
-      marginRight: "17px",
-      boxShadow: "0px 2px 11px #9E1F0026",
-    },
-    rowView: {
-      alignSelf: "stretch",
-      display: "flex",
-      alignItems: "center",
-      marginBottom: "12px",
-    },
-    button: {
-      width: "51px",
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      background: "#816043",
-      borderRadius: "8px",
-      border: "none",
-      paddingTop: "8px",
-      paddingBottom: "8px",
-      marginRight: "8px",
-      textAlign: "left",
-      color: "#FFFFFF",
-      fontSize: "12px",
-      fontWeight: "bold",
-    },
-    treeImage: {
-      position: "absolute",
-      zIndex: 1,
-      width: "338px",
-      height: "271px",
-      marginLeft: "27px",
-      marginRight: "30px",
-      marginTop: "110px",
-    },
-    gifImage: {
-      position: "relative",
-      zIndex: 2,
-      width: "255px",
-      height: "297px",
-      marginLeft: "80px",
-    },
+const PasswordChangeScreen = () => {
+  const navigate = useNavigate();
+
+  const [currentPassword, setCurrentPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [showCurrentPassword, setShowCurrentPassword] = useState(false);
+  const [showNewPassword, setShowNewPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+  const isButtonDisabled =
+    !currentPassword || !newPassword || newPassword !== confirmPassword;
+
+  const commonInputContainerStyle = {
+    alignSelf: "stretch",
+    display: "flex",
+    alignItems: "center",
+    borderRadius: 8,
+    border: "1px solid #D1D3D8",
+    paddingLeft: 18,
+    paddingRight: 18,
+    marginLeft: 20,
+    marginRight: 20,
+  };
+
+  const togglePasswordVisibility = (type) => {
+    if (type === "current") {
+      setShowCurrentPassword(!showCurrentPassword);
+    } else if (type === "new") {
+      setShowNewPassword(!showNewPassword);
+    } else if (type === "confirm") {
+      setShowConfirmPassword(!showConfirmPassword);
+    }
+  };
+
+  const handleChangePassword = async () => {
+    try {
+      const response = await customAxios.put("/members/update-pw", {
+        oldPassword: currentPassword,
+        newPassword: newPassword,
+      });
+      console.log("비밀번호변경: ", response);
+      navigate(-1);
+      toast.success("비밀번호 변경 완료!", {
+        duration: 1000,
+      });
+    } catch (error) {
+      console.warn("비밀번호변경", error);
+    }
   };
 
   return (
-    <div style={styles.scrollView}>
-      <div style={styles.column}>
-        <div style={styles.rowView}>
-          <button style={styles.button} onClick={() => alert("Pressed!")}>
-            F1 - I
-          </button>
-          <span style={{ color: "#212124", fontSize: "14px", fontWeight: "bold" }}>
-            총 누적 경험치
-          </span>
-          <div style={{ flex: 1 }}></div>
-          <span style={{ color: "#816043", fontSize: "14px", fontWeight: "bold" }}>
-            12,657
-          </span>
-          <span style={{ color: "#868B94", fontSize: "14px", fontWeight: "bold" }}>
-            /
-          </span>
-          <span style={{ color: "#868B94", fontSize: "14px", fontWeight: "bold" }}>
-            13,500
-          </span>
-          <span style={{ color: "#868B94", fontSize: "14px", fontWeight: "bold" }}>
-            do
-          </span>
-        </div>
-        <div style={{ background: "#DCDEE3", borderRadius: "25px", height: "12px" }}>
-          <div
-            style={{
-              background: "#816043",
-              width: "85%",
-              height: "100%",
-              borderRadius: "25px",
-            }}
-          ></div>
-        </div>
+    <div className="page" style={styles.container}>
+      <div style={styles.headerContainer}>
+        <PressableButton
+          onClick={() => navigate(-1)}
+          pressedStyle={{ opacity: 0.5 }}
+        >
+          <img src={arrowback} style={styles.arrowback} alt="arrowback" />
+        </PressableButton>
+        <span className="title-3-bold">비밀번호 변경</span>
       </div>
-      <div>
-        <img src={HomeTree} alt="Tree Background" style={styles.treeImage} />
-        <img src={gif1} alt="Animated character" style={styles.gifImage} />
+      <span style={styles.label}>{"현재 비밀번호"}</span>
+      <div
+        style={{
+          ...commonInputContainerStyle,
+          marginBottom: 12,
+        }}
+      >
+        <input
+          type={showCurrentPassword ? "text" : "password"}
+          placeholder={"현재 비밀번호"}
+          value={currentPassword}
+          onChange={(event) => setCurrentPassword(event.target.value)}
+          style={styles.input}
+        />
+        <img
+          src={showCurrentPassword ? visibilityoff : visibility}
+          style={styles.eyeIcon}
+          alt="Eye Icon"
+          onClick={() => togglePasswordVisibility("current")}
+        />
       </div>
-      <div className="absolute-column">
-        <div className="row-view">
-          <span className="text">최근 획득한 경험치</span>
-          <span className="text2">자세히보기</span>
-          <img src={dart} className="image" alt="Detail Icon" />
-        </div>
-        <div className="row-view2">
-          <div className="view">
-            <img src={dart} className="image2" alt="Icon" />
-          </div>
-          <div className="column">
-            <div className="row-view3">
-              <span className="text3">월특근</span>
-              <div className="view2">
-                <span className="text4">리더부여</span>
-              </div>
-            </div>
-            <div className="row-view4">
-              <div className="view3">
-                <span className="text5">MAX 달성</span>
-              </div>
-              <div className="view4">
-                <span className="text6">1월</span>
-              </div>
-            </div>
-            <div className="row-view5">
-              <span className="text7">2024.05.12</span>
-              <img src={dart} className="image3" alt="Divider" />
-              <span className="text8">5회</span>
-            </div>
-          </div>
-          <span className="text9">100</span>
-          <span className="text10">do</span>
-        </div>
+      <span style={styles.label}>{"새 비밀번호"}</span>
+      <div
+        style={{
+          ...commonInputContainerStyle,
+          marginBottom: 12,
+        }}
+      >
+        <input
+          type={showNewPassword ? "text" : "password"}
+          placeholder={"새 비밀번호"}
+          value={newPassword}
+          onChange={(event) => setNewPassword(event.target.value)}
+          style={styles.input}
+        />
+        <img
+          src={showNewPassword ? visibilityoff : visibility}
+          style={styles.eyeIcon}
+          alt="Eye Icon"
+          onClick={() => togglePasswordVisibility("new")}
+        />
       </div>
+      <div
+        style={{
+          ...commonInputContainerStyle,
+          marginBottom: 24,
+        }}
+      >
+        <input
+          type={showConfirmPassword ? "text" : "password"}
+          placeholder={"새 비밀번호 확인"}
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+          style={styles.input}
+        />
+        <img
+          src={showConfirmPassword ? visibilityoff : visibility}
+          style={styles.eyeIcon}
+          alt="Eye Icon"
+          onClick={() => togglePasswordVisibility("confirm")}
+        />
+      </div>
+      <button
+        style={isButtonDisabled ? styles.disabledButton : styles.button}
+        onClick={handleChangePassword}
+        disabled={isButtonDisabled}
+      >
+        <span
+          style={{
+            color: isButtonDisabled ? colors.gray[500] : "#FFFFFF", // 텍스트 색상 동적 설정
+            fontSize: 18,
+            fontWeight: "bold",
+          }}
+        >
+          {"비밀번호 변경"}
+        </span>
+      </button>
     </div>
   );
 };
 
-export default HomeScreen;
+const styles = {
+  container: {
+    display: "flex",
+    alignItems: "flex-start",
+  },
+  headerContainer: {
+    alignSelf: "stretch",
+    display: "flex",
+    alignItems: "center",
+    marginLeft: 30,
+    marginRight: 17,
+    marginTop: 3,
+    width: 390,
+    height: 45,
+    marginBottom: 5,
+  },
+  arrowback: {
+    width: 10,
+    height: 18,
+    marginRight: 107,
+    objectFit: "fill",
+  },
+  label: {
+    color: colors.gray[900],
+    fontSize: 14,
+    fontWeight: 700,
+    marginBottom: 15,
+    marginLeft: 21,
+    marginTop: 12,
+  },
+  input: {
+    color: colors.gray[600],
+    fontSize: 16,
+    marginRight: 4,
+    flex: 1,
+    alignSelf: "stretch",
+    background: "none",
+    border: "none",
+    paddingTop: 13,
+    paddingBottom: 13,
+  },
+  eyeIcon: {
+    width: 24,
+    height: 24,
+    objectFit: "fill",
+    cursor: "pointer",
+  },
+  button: {
+    alignSelf: "stretch",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    background: colors.orange[500],
+    borderRadius: 8,
+    border: "none",
+    paddingTop: 18,
+    paddingBottom: 18,
+    marginBottom: 50,
+    marginLeft: 20,
+    marginRight: 20,
+    textAlign: "left",
+  },
+  disabledButton: {
+    alignSelf: "stretch",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    background: colors.gray[300],
+    borderRadius: 8,
+    border: "none",
+    paddingTop: 18,
+    paddingBottom: 18,
+    marginBottom: 50,
+    marginLeft: 20,
+    marginRight: 20,
+    textAlign: "left",
+  },
+};
+
+export default PasswordChangeScreen;
